56a670348d7ae53b849b3f632d93c7a27490c525
Mahboob Alam <mahboob30699@gmail.com>
2024-03-11 21:01:18 +0500

    Price Range

--- src/component/FormInput.jsx
+++ src/component/FormInput.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-const FormInput = ({type,label,name, defaultValue}) => {
+const FormInput = ({type,label,name, defaultValue, size}) => {
   console.log(label)
   return (
     <label className="form-control">
       <div className="label">
-        <span className="label-text">{label}</span>
+        <span className="label-text capitalize">{label}</span>
       </div>
       <input
         type={type}
         name ={name}
         defaultValue={defaultValue}
-        className="input input-bordered "
+        className={`input input-bordered ${size}`}
       />
       
     </label>
